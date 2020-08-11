950c98099af2d1492f57bf256f424e3340489bfe
Chase Michael <chasez95@gmail.com>
2020-08-10 21:59:34 -0400

    Add practice set 0

---
+++ practice_set_0.py
@@ -0,0 +1,11 @@
+import math
+
+
+def main():
+    x = int(input('Enter number x: '))
+    y = int(input('Enter number y: '))
+    print('x**y =', (x ** y))
+    print('log(x) =', math.log(x))
+
+
+main()
